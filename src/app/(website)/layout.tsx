13239e3f1d575f797d0d2969b62ad552c731f204
Monir Hossain <monirhrabby.personal@gmail.com>
2025-03-19 19:42:56 +0600

    project init

---
+++ src/app/(website)/layout.tsx
@@ -0,0 +1,11 @@
+export default function WebsiteLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <div>
+        {children}
+      </div>
+    );
+  }
