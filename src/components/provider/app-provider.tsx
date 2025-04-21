f3aa87e3655fb8c1dfce370ef4e48e1b358c8226
ILhankhondaker <ilhankhondaker@gmail.com>
2025-04-21 15:19:21 +0600

    toas fixup

--- src/components/provider/app-provider.tsx
+++ src/components/provider/app-provider.tsx
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
-import { Toaster } from 'sonner';
 
 interface Props {
   children: ReactNode;
@@ -13,7 +12,7 @@ const AppProvider = ({ children }: Props) => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      {children} <Toaster richColors position="top-right" />
+      {children} 
     </QueryClientProvider>
   );
 };
