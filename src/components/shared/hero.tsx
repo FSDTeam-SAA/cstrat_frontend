8a34bb173b2b5eedc39d2d935e9990a3f5a3c980
mehedishishir0 <mehedihasanshishir@gmail.com>
2025-07-30 15:14:03 +0600

    add

--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -12,12 +12,12 @@ export default function Hero() {
     >
       <section className="flex h-full w-full flex-col items-center justify-center bg-white/50 px-4">
         <div className="container mx-auto py-16 md:py-24">
-          <div className="max-w-[688px] space-y-6 text-center">
+          <div className="max-w-[688px] space-y-6 ">
             <h1 className="title">Elevate Your Brand with Drip-Worthy Swag</h1>
             <p className="text-lg text-black">
               Premium, Custom Merch That Turns Heads and Builds Loyalty.
             </p>
-            <div className="flex flex-col gap-4 sm:flex-row sm:justify-center">
+            <div className="flex flex-col gap-4 sm:flex-row ">
               <Button asChild size="lg" className="bg-black text-white hover:bg-gray-800">
                 <Link href="/shop">Shop Curated Collections</Link>
               </Button>
